88141c4577ff1d8c1ff87179e7001a83784473f1
Nick McComb <nick@nickmccomb.net>
2016-10-08 16:42:22 -0700

    Lab 2 | Implemented Multiplexing LEDs

--- lab2/src/lab2.c
+++ lab2/src/lab2.c
@@ -9,11 +9,29 @@
 //  PORTB bits 4-6 go to a,b,c inputs of the 74HC138.
 //  PORTB bit 7 goes to the PWM transistor base.
 
-#define F_CPU 16000000 // cpu speed in hertz 
+//#define F_CPU 16000000 // cpu speed in hertz 
 #define TRUE 1
 #define FALSE 0
 #include <avr/io.h>
 #include <util/delay.h>
+#include <stddef.h>
+
+
+//Segment pin definitions
+#define SEG_A  0x01
+#define SEG_B  0x02
+#define SEG_C  0x04
+#define SEG_D  0x08
+#define SEG_E  0x10
+#define SEG_F  0x20
+#define SEG_G  0x40
+#define SEG_DP 0x80
+
+//PORTB Definitions
+#define DIG_SEL_1 0x10
+#define DIG_SEL_2 0x20
+#define DIG_SEL_3 0x40
+#define PWM_CTRL 0x80
 
 //holds data to be sent to the segments. logic zero turns segment on
 uint8_t segment_data[5];
@@ -21,6 +39,15 @@ uint8_t segment_data[5];
 //decimal to 7-segment LED display encodings, logic "0" turns on segment
 uint8_t dec_to_7seg[12];
 
+//Digit control low-level code
+void inline SET_DIGIT_ONE(void)   {PORTB |= DIG_SEL_3; PORTB = PORTB & ~(DIG_SEL_1 | DIG_SEL_2);}
+void inline SET_DIGIT_TWO(void)   {PORTB |= DIG_SEL_1 | DIG_SEL_2; PORTB = PORTB & ~(DIG_SEL_3);}
+void inline SET_DIGIT_THREE(void) {PORTB |= DIG_SEL_1; PORTB = PORTB & ~(DIG_SEL_2 | DIG_SEL_3);}
+void inline SET_DIGIT_FOUR(void)  {PORTB = PORTB & ~(DIG_SEL_1 | DIG_SEL_2 | DIG_SEL_3);}
+
+void configureIO( void );
+void setSegment( uint16_t targetOutput );
+void setDigit( uint8_t targetDigit );
 
 //******************************************************************************
 //                            chk_buttons                                      
@@ -39,6 +66,8 @@ uint8_t chk_buttons(uint8_t button) {
 //takes a 16-bit binary input value and places the appropriate equivalent 4 digit 
 //BCD segment code in the array segment_data for display.                       
 //array is loaded at exit as:  |digit3|digit2|colon|digit1|digit0|
+
+  return 0;
 }
 
 void segsum(uint16_t sum) {
@@ -49,12 +78,108 @@ void segsum(uint16_t sum) {
 }//segment_sum
 //***********************************************************************************
 
+void configureIO( void ){
+  DDRA = 0xFF; //Initialize DDRA as if we want to control the LEDs
+  DDRB = 0xF0; //Upper nibble of the B register is for controlling
+
+  //For this lab, we are just driving the PWM_CTRL line low always
+  //PORTB |= PWM_CTRL;  
+  //(it defaults to low)
+	
+}
+
+
+void setSegment( uint16_t targetOutput ){
+  switch(targetOutput){
+     case 0:
+       PORTA = ~(SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F);
+       break;
+     case 1:
+       PORTA = ~(SEG_B | SEG_C);
+       break;
+     case 2:
+       PORTA = ~(SEG_A | SEG_B | SEG_D | SEG_E | SEG_G);
+       break;
+     case 3:
+       PORTA = ~(SEG_A | SEG_B | SEG_C | SEG_D | SEG_G);  //Changed G to E
+       break;
+     case 4:
+       PORTA = ~(SEG_B | SEG_C | SEG_F | SEG_G);
+       break;
+     case 5:
+       PORTA = ~(SEG_A | SEG_C | SEG_D | SEG_F | SEG_G);
+       break;
+     case 6:
+       PORTA = ~(SEG_A | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G);
+       break;
+     case 7:
+       PORTA = ~(SEG_A | SEG_B | SEG_C);
+       break;
+     case 8:
+       PORTA = ~(SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G);
+       break;
+     case 9:
+       PORTA = ~(SEG_A | SEG_B | SEG_C | SEG_F | SEG_G);
+       break;
+     case 10: //A
+       break; 
+     case 11: //B
+       break;
+      
+  }
+ 
+}
+
+void setDigit( uint8_t targetDigit ){ 
+  switch(targetDigit){
+    case 1:
+      SET_DIGIT_ONE();
+      break;
+    case 2:
+      SET_DIGIT_TWO();
+      break;
+    case 3:
+      SET_DIGIT_THREE();
+      break;
+    case 4:
+      SET_DIGIT_FOUR();
+      break;
+  }
+
+}
 
 //***********************************************************************************
 int main()
 {
 //set port bits 4-7 B as outputs
 while(1){
+  configureIO();
+  //_delay_ms(300);
+
+  //PORTB = PORTB & ~(DIG_SEL_1 | DIG_SEL_2 | DIG_SEL_3);  //Select decoder 0 output (enable digit 4)
+
+  PORTB |= DIG_SEL_1 | DIG_SEL_2;
+
+  SET_DIGIT_FOUR();
+
+  //setSegment(2);
+
+  int i, j, k;
+
+  while(1){
+    for(i = 0; i < 10; ++i){
+      setSegment(i);
+      //_delay_ms(250);
+      for(k = 0; k < 100; ++k){
+        for(j = 1; j < 5; ++j){
+          setDigit(j);
+	  _delay_ms(1);
+        }
+      }
+
+    }
+  }
+  
   //insert loop delay for debounce
   //make PORTA an input port with pullups 
   //enable tristate buffer for pushbutton switches
